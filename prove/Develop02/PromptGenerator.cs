c20b013724922272abdfa68d90d9bac1ebc6822e
byui-titus <Titusrayz33@gmail.com>
2024-05-01 11:14:16 -0600

    ex

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -26,7 +26,15 @@ class PromptGenerator {
             "Believe you can and you're halfway there. - Theodore Roosevelt",
             "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
             "You are never too old to set another goal or to dream a new dream. - C.S. Lewis",
-            "The secret of getting ahead is getting started. - Mark Twain"
+            "The secret of getting ahead is getting started. - Mark Twain",   
+            "We cannot solve problems with the kind of thinking we employed when we came up with them. —Albert Einstein",
+            "Learn as if you will live forever, live like you will die tomorrow.” —Mahatma Gandhi",
+            "Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.—Mark Twain",
+            "When you give joy to other people, you get more joy in return. You should give a good thought to the happiness that you can give out. —Eleanor Roosevelt",
+            "When you change your thoughts, remember to also change your world. —Norman Vincent Peale",
+            "It is only when we take chances that our lives improve. The initial and the most difficult risk we need to take is to become honest. —Walter Anderson",
+            "Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together. —Diane McLaren"
+
         };
     }
 
